eea7e97e2dd1e4e861fbd08db561e82e416ecbbc
carshy <mussocollins@gmail.com>
2024-07-09 11:37:32 +0300

    Change querries for fetching entries

--- backend/journals/views.py
+++ backend/journals/views.py
@@ -3,12 +3,12 @@ from .models import JournalEntry
 from .serializers import JournalEntrySerializer
 
 class JournalEntryViewSet(viewsets.ModelViewSet):
-    # queryset = JournalEntry.objects.all()
+    queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
-        return JournalEntry.objects.filter(user=self.request.user)
+    # def get_queryset(self):
+    #     return JournalEntry.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
